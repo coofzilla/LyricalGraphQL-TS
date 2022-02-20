2c9cc7733e26234f07c3fedcfe8383f0d280ffd5
coofzilla <jeric614@yahoo.com>
2022-02-20 11:45:41 +0900

    setup f/apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,22 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 import App from "./App";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
