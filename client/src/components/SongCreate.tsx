9e96bed03fdfc95ac9b8949b5bd9a884f8051785
coofzilla <jeric614@yahoo.com>
2022-02-22 09:04:35 +0900

    addSong mutation f/songCreate

--- client/src/components/SongCreate.tsx
+++ client/src/components/SongCreate.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
 
 interface SongCreateProps {
   someProp?: any;
 }
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
 
 const SongCreate = ({ someProp }: SongCreateProps) => {
   const [input, setInput] = useState("");
+  const [addSong, { data, loading, error }] = useMutation(mutation);
+  console.log(data);
+
+  const onSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    addSong({ variables: { title: input } });
+  };
 
   return (
     <div>
-      <h1>Create a New Song</h1>
-      <form>
+      <h3>Create a New Song</h3>
+      <form onSubmit={onSubmitHandler}>
         <label>Song Title: </label>
         <input
           type="text"
